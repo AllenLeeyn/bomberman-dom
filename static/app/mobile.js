6dc0f7b9601a1ab86b955947f7b25806dfc57e1d
ylee <leeyn.shun@gmail.com>
2025-07-28 18:56:36 +0300

    change mobile controls layout and responsive setting again...

--- static/app/mobile.js
+++ static/app/mobile.js
@@ -42,7 +42,7 @@ export function initMobileControls() {
     let dx = touch.clientX - touchStart.x;
     let dy = touch.clientY - touchStart.y;
 
-    let threshold = 2;
+    let threshold = 6;
     let direction = null;
 
     if (Math.abs(dx) > Math.abs(dy)) {
@@ -59,8 +59,8 @@ export function initMobileControls() {
         action: "game",
         content: JSON.stringify([currentDirection]),
       });
-      touchStart = { x: touch.clientX, y: touch.clientY };
     }
+    touchStart = { x: touch.clientX, y: touch.clientY };
   };
 
   joystickEndHandler = function(e) {
